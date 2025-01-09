db8130d039f0bf38d8600c0d93c241257e69edc4
Samuel Ologunowa <samueloluwatommzy@gmail.com>
2025-01-09 22:55:23 +0000

    add more wait time

--- src/__tests__/controllers/userController.test.ts
+++ src/__tests__/controllers/userController.test.ts
@@ -21,13 +21,13 @@ beforeAll(async () => {
 	} catch (error) {
 		console.log({ err: error });
 	}
-}, 30);
+}, 30000);
 
 afterAll(async () => {
 	// Database teardown
 	console.log("Running this after all");
-	DatabaseConnection.dropDatabase();
-	DatabaseConnection.close();
+	await DatabaseConnection.dropDatabase();
+	await DatabaseConnection.close();
 });
 describe("User controller operations", () => {
 	const token = jwt.sign({ _id: "6778627d2724c156d2a2a9e7" }, process.env.JWT_SECRET as string);
