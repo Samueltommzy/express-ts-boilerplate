ce1d6f216306d72b4607480fe4d5c1f90d90848a
Samuel Ologunowa <samueloluwatommzy@gmail.com>
2025-02-11 22:47:27 +0000

    Trigger new build

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -91,9 +91,7 @@ class UserService implements IUserService {
 	public async updateUser(userId: string, decodedUserId: string, user: IUser): Promise<IUser> {
 		try {
 			await this.getUser(userId, decodedUserId);
-			// if(!userData){
-			// 	throw new ResourceNotFoundException("User not found");
-			// }
+
 			await this.userRepository.update(userId, user);
 			return (await this.userRepository.findById(userId)) as IUser;
 		} catch (err) {
